c57b8971e1afa7ab2147fbd08f27bc0ba2cf032d
cookie <whdrnr01@gmail.com>
2023-09-07 17:56:22 +0900

    Alpinejs Nav Example Practice

---
+++ menu/index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="ko">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Alpinejs Nav Full Example</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" referrerpolicy="no-referrer" />
+	<script>
+		<!-- Start with Document Loaded -->
+		document.addEventListener('alpine.init', () => {
+			
+		})
+	</script>	
+</head>
+<body>
+	<div class="flex min-h-[720px] bg-white">
+		<div class="flex w-72 felx-col">
+			<?php include_once "sidebar.html"; ?>	
+		</div>
+	</div>
+	
+
+
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.0/dist/cdn.min.js"></script>
+</body>
+</html>
